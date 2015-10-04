ca94d1cde3fb238a16ef3a203249b80dc1440f75
hdsheena <hdsheena@gmail.com>
2015-10-04 08:51:33 -0700

    added documentation

--- printBooklet.py
+++ printBooklet.py
@@ -1,15 +1,21 @@
+#make this into an input request
 number_of_pages = 12
+
 list_of_pages = []
-list = []
+
+#checks if page number will work for printing 4 sheets to a page
 if number_of_pages%4 == 0:
 	middle_page = number_of_pages/2
 else:
 	print "need " + str(number_of_pages%4) + "more pages"
 
-
+#takes the end pages and puts them in order
 for n in range(middle_page+1, number_of_pages+1):
 	list_of_pages.insert(0, n)
 	
+
+#sticks the beginning pages in the end page list in order
+
 location = 0
 n = 0
 while n < middle_page:
@@ -19,6 +25,9 @@ while n < middle_page:
 	n = n+1
 	list_of_pages.insert(location+1, n)
 	location = location+3
+
+#prints out the list
 print list_of_pages
 
-20,1,2,19,18,3,4,17,16,5,6,15,14,7,8,13,12,9,10,11
+#correct list for 20 pages
+#20,1,2,19,18,3,4,17,16,5,6,15,14,7,8,13,12,9,10,11
